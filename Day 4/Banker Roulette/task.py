c46a77544d4290092048a5f769ffb54b267c82ce
jasikhalid <jasikhalid46@gmail.com>
2025-11-12 14:00:14 +0530

    Add Day 5 - Rock Paper Scissors

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,5 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+rand=random.randint(0,(len(friends)-1))
+print(friends[rand])
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +25,52 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+rps=random.randint(0,2)
+yrps=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+if yrps == 0:
+    print("You choose rock")
+    print(rock)
+    if rps==1:
+        print("Computer choose paper")
+        print(paper)
+        print("Computer wins")
+    elif rps==2:
+        print("Computer choose scissors")
+        print(scissors)
+        print("You wins")
+    else:
+        print("Computer choose rock")
+        print(rock)
+        print("Draw")
+elif yrps==1:
+    print("You choose paper")
+    print(paper)
+    if rps == 1:
+        print("Computer choose paper")
+        print(paper)
+        print("Draw")
+    elif rps == 2:
+        print("Computer choose scissors")
+        print(scissors)
+        print("Computer wins")
+    else:
+        print("Computer choose rock")
+        print(rock)
+        print("You wins")
+elif yrps==2:
+    print("You choose scissors")
+    print(scissors)
+    if rps == 1:
+        print("Computer choose paper")
+        print(paper)
+        print("You wins")
+    elif rps == 2:
+        print("Computer choose scissors")
+        print("Draw")
+    else:
+        print("Computer choose rock")
+        print(rock)
+        print("Computer wins")
+else:
+    print("You choose a invalid number,You LOOSE")
+
